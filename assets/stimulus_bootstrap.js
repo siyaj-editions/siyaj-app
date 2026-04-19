987c2ef6dc85a557621733f7a3d01fa9fcafb13e
Ludwig ELATRE <ludwig.elatre@outlook.com>
2026-04-19 18:56:42 +0200

    feat(design): refine editorial heroes and typography

--- assets/stimulus_bootstrap.js
+++ assets/stimulus_bootstrap.js
@@ -3,6 +3,7 @@ import AutocompleteController from './controllers/autocomplete_controller.js';
 import CollectionController from './controllers/collection_controller.js';
 import CookieConsentController from './controllers/cookie_consent_controller.js';
 import DatePickerController from './controllers/date_picker_controller.js';
+import EditorialTabsController from './controllers/editorial_tabs_controller.js';
 import FormFeedbackController from './controllers/form_feedback_controller.js';
 import UploadDropzoneController from './controllers/upload_dropzone_controller.js';
 
@@ -12,5 +13,6 @@ app.register('autocomplete', AutocompleteController);
 app.register('collection', CollectionController);
 app.register('cookie-consent', CookieConsentController);
 app.register('date-picker', DatePickerController);
+app.register('editorial-tabs', EditorialTabsController);
 app.register('form-feedback', FormFeedbackController);
 app.register('upload-dropzone', UploadDropzoneController);
